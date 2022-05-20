7b04534e1a81b511c7677fdc7621f5e2afd81f31
Enes <54986652+altunenes@users.noreply.github.com>
2022-05-21 00:29:40 +0300

    Update Filelistfromfolder.py

--- E_Prime_Scripts/Filelistfromfolder.py
+++ E_Prime_Scripts/Filelistfromfolder.py
@@ -3,8 +3,8 @@
                                                               
                                                                     #Enes Altun 05/21/2022
                                               #This script saves all the names of visual stimuli in a folder as a CSV.
-                                                           #Change the 17th line to find stim directory
-#Since SHINE TOOLBOX ads "SHINEd" in front of all image names I also added a condition to remove this. You can remove the lines 27 to 30 if you want to hold "SHINEd"
+                                                           #Change the 19th line to find stim directory
+#Since SHINE TOOLBOX ads "SHINEd" in front of all image names I also added a condition to remove this. You can remove the lines 28 to 32 if you want to hold "SHINEd"
       
 # ==========================================================================================================================================================
 
@@ -13,7 +13,9 @@ import os
 import pandas as pd
 
 path = os.getcwd()
-print(path)
+print('Working directory: ' + path)
+
+# write directory
 folder_name = r'C:\Users\altunenes\Desktop\shined_all'
 folder_name_save = 'anoutputfolder'
 path_folder = os.path.join(path, folder_name)
@@ -33,4 +35,5 @@ for i in list_files:
 #save the list as csv
 df = pd.DataFrame(list_files_new)
 df.to_csv(os.path.join(path_folder_save, 'list_files.csv'), index=False)
-
+print('Saved directory: ' + path_folder_save)
+print('Number of the images: ' + str(len(list_files_new)))
