595a647374e5d83f3042cea8024656c7dc64d493
Enes <54986652+altunenes@users.noreply.github.com>
2022-06-17 17:39:41 +0300

    Update e_prime_bullshit.py

--- E_Prime_Scripts/e_prime_bullshit.py
+++ E_Prime_Scripts/e_prime_bullshit.py
@@ -8,4 +8,4 @@ def convert_to_bmp(path, new_path):
         img = cv2.imread(os.path.join(path, file))
         cv2.imwrite(os.path.join(new_path, file[:-3] + 'bmp'), img)
         
-convert_to_bmp(r'C:\Users\altunenes\Desktop\imagefolder', r'C:\Users\Yol Gezer\Desktop\exportfolder')
+convert_to_bmp(r'C:\Users\altunenes\Desktop\imagefolder', r'C:\Users\altunenes\Desktop\exportfolder')
