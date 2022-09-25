4251504c1f05ac1aa02856b84d463e0d8a842c74
Enes <54986652+altunenes@users.noreply.github.com>
2022-09-26 02:37:25 +0300

    Update img_pixels.py

--- scripts_4_experiments/img_pixels.py
+++ scripts_4_experiments/img_pixels.py
@@ -24,3 +24,33 @@ cv2.setMouseCallback('image',pixel_value)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+################################################################### V2 ##################################
+##################################### V2, Right clic BGR Values, Left click dimensions (with mouse) #############################
+# import cv2
+# import numpy as np
+
+# def click_event(event, x, y, flags, param):
+#     if event == cv2.EVENT_LBUTTONDOWN:
+#         print(x,', ',y)
+#         font = cv2.FONT_HERSHEY_SIMPLEX
+#         strXY = str(x) + ', ' + str(y)
+#         cv2.putText(img, strXY, (x,y), font, .4, (255,255,0), 2)
+#         cv2.imshow('image', img)
+#     if event == cv2.EVENT_RBUTTONDOWN:
+#         blue = img[y,x,0]
+#         green = img[y,x,1]
+#         red = img[y,x,2]
+#         font = cv2.FONT_HERSHEY_SIMPLEX
+#         strBGR = str(blue) + ', ' + str(green) + ', ' + str(red)
+#         cv2.putText(img, strBGR, (x,y), font, .4, (0,255,255), 2)
+#         cv2.imshow('image', img)
+
+# #img = np.zeros((512,512,3), np.uint8)
+# img = cv2.imread('FFT.png')
+# cv2.imshow('image', img)
+
+# cv2.setMouseCallback('image', click_event)
+
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
