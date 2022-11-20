58d055a46b08a71e67100b8d13e9470eebf9c19b
Enes <54986652+altunenes@users.noreply.github.com>
2022-11-21 00:17:19 +0300

    Update img_pixels.py

--- scripts_4_experiments/img_pixels.py
+++ scripts_4_experiments/img_pixels.py
@@ -1,56 +1,41 @@
-############################################################### under construction ############################################################
 
+############################################################## Enes Altun 21.11.2022  ##############################################################
+###############################################################  Print RGB VALUES ############################################################
+################################## Usage: Import the image, when the image is displayed, press the left mouse button to print the coordinates of the pixel, press the right mouse button to print the RGB values of the pixel. ##################################
+######################################################### RGB values are printed in the terminal with the mouse movement. #########################################################
 import cv2
-import numpy as np
 
-def pixel_value(event,x,y,flags,param):
+def click_event(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDOWN:
-        print(img[y,x])
-
+        print(x,', ',y)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        strXY = str(x) + ', ' + str(y)
+        cv2.putText(img, strXY, (x,y), font, .4, (255,255,0), 2)
+        cv2.imshow('image', img)
+    if event == cv2.EVENT_RBUTTONDOWN:
         blue = img[y,x,0]
         green = img[y,x,1]
         red = img[y,x,2]
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        strBGR = str(blue) + ', ' + str(green) + ', ' + str(red)
+        cv2.putText(img, strBGR, (x,y), font, .4, (0,255,255), 2)
+        cv2.imshow('image', img)
+#also print the rgb valus in real time in the terminal mouse is over
+    if event == cv2.EVENT_MOUSEMOVE:
+        blue = img[y,x,0]
+        green = img[y,x,1]
+        red = img[y,x,2]
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        strBGR = str(blue) + ', ' + str(green) + ', ' + str(red)
+        #print on terminal
+        print(strBGR)
+        
+        
+img = cv2.imread('output.png')
+cv2.imshow('image', img)
 
-        cv2.circle(img,(x,y),3,(0,0,255),-1)
-        mycolorImage = np.zeros((512,512,3),np.uint8)
-
-        mycolorImage[:] = [blue,green,red]
-
-        cv2.imshow('color',mycolorImage)
-
-img = cv2.imread('Lena.png')
-cv2.imshow('image',img)
-cv2.setMouseCallback('image',pixel_value)
+cv2.setMouseCallback('image', click_event)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-################################################################### V2 ##################################
-##################################### V2, Right clic BGR Values, Left click dimensions (with mouse) #############################
-# import cv2
-# import numpy as np
-
-# def click_event(event, x, y, flags, param):
-#     if event == cv2.EVENT_LBUTTONDOWN:
-#         print(x,', ',y)
-#         font = cv2.FONT_HERSHEY_SIMPLEX
-#         strXY = str(x) + ', ' + str(y)
-#         cv2.putText(img, strXY, (x,y), font, .4, (255,255,0), 2)
-#         cv2.imshow('image', img)
-#     if event == cv2.EVENT_RBUTTONDOWN:
-#         blue = img[y,x,0]
-#         green = img[y,x,1]
-#         red = img[y,x,2]
-#         font = cv2.FONT_HERSHEY_SIMPLEX
-#         strBGR = str(blue) + ', ' + str(green) + ', ' + str(red)
-#         cv2.putText(img, strBGR, (x,y), font, .4, (0,255,255), 2)
-#         cv2.imshow('image', img)
-
-# #img = np.zeros((512,512,3), np.uint8)
-# img = cv2.imread('FFT.png')
-# cv2.imshow('image', img)
-
-# cv2.setMouseCallback('image', click_event)
-
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
